d9b493c7071a5ad1c28fdcaca31389ab58eddc91
Burhan <burhan221b@gmail.com>
2019-01-13 00:50:15 -0800

    Added some styles and pictures now included... excluded searches with no image.

--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -6,11 +6,20 @@ import { Link } from 'react-router-dom';
 const SeriesListItem = ({ series }) => (
 
     <li>
-        {(series.show.image == null) ? series.show.image = "http://static.tvmaze.com/uploads/images/medium_portrait/116/291721.jpg" : true}
-        <Link to={`/series/${series.show.id}`}>{series.show.name}</Link>
-        <img alt="Show" src={series.show.image.medium} />
+        {/* {(series.show.image == null) ? series.show.image = "http://static.tvmaze.com/uploads/images/medium_portrait/116/291721.jpg" : null} */}
+
+        {/* <Link className='li-list' to={`/series/${series.show.id}`}>{series.show.name}</Link> */}
+        {/* {(series.show.image == null) ? series.show.image = "" : null}
+        <img className="series-list-image" alt="Show" src={(series.show.image.medium === 'undefined') ? series.show.image : series.show.image.medium} /> */}
+
+        {(series.show.image == null) ? null : <Link className='li-list' to={`/series/${series.show.id}`}>{series.show.name}</Link>}
+        {(series.show.image == null) ? null : [<img className="series-list-image" alt="Show" src={(series.show.image == null) ? series.show.image = '' : [(series.show.image.medium === 'undefined') ? series.show.image : series.show.image.medium]} />]}
+
+
+        {/* {console.log(series)} */}
+        {/* {console.log(series.show.image)} */}
         {/* <img alt="Show" src={typeof (series.show.image) !== false ? series.show.image.medium : "http://static.tvmaze.com/uploads/images/medium_portrait/116/291721.jpg"} /> */}
-        {console.log(series)}
+
         {/* {console.log(series.show.image.medium)} */}
         {/* {console.log(typeof (series.show.image.medium))} */}
     </li >
